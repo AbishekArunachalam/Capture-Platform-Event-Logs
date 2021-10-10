060472847e53dd776f99c34fc923dc1ac3f65bcf
abishekarunachalam <abs.abs105@gmail.com>
2021-10-10 22:35:06 +1100

    Added doc strings and unittests

--- src/record_event.py
+++ src/record_event.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
-import os
 import json
 import configparser
 import pandas as pd
-import sqlalchemy as db
 from sqlalchemy import exc
+from sqlalchemy import create_engine, text
 from sqlalchemy_utils import database_exists, create_database
 
 
-class EventCapture:
-
+class EventCapture():
     def __init__(self, hostname, user_name, password, db_name, table_name, port):
         self.hostname = hostname
         self.user_name = user_name
@@ -20,48 +18,88 @@ class EventCapture:
         self.port = port
 
     def initialise_db_connection(self):
+        """creates DB connection object
+
+        Returns:
+            object: db connection
+        """
         try:
-            engine = db.create_engine(
-                f'mysql+pymysql://{self.user_name}:{self.password}@{self.hostname}:{self.port}/{self.db_name}')
+            engine = create_engine(
+                f"mysql+pymysql://{self.user_name}:{self.password}@{self.hostname}:{self.port}/{self.db_name}")
 
             if not database_exists(engine.url):
                 create_database(engine.url)
-            print("MySQL Database connection successful")
+            print("MySQL Database connection established successfully")
 
         except exc.SQLAlchemyError as err:
-            print(err)
+            print(
+                f"Exception encountered while establishing connection to server: {str(err)}")
+            raise
 
         return engine
 
-    def read_json(self):
-        path = "../data/event-sample.json"
-        with open(path, "r") as f:
-            data = json.loads(f.read())
-            data_dict = dict()
-            for i in range(0, len(data)):
-                list_item = data[i]
-                payload_id = list_item["Payload"]["ID"]
-                event_type = list_item["Type"]
-                event_recorded_at = list_item["RecordedAt"]
-                data_dict[i] = {
-                    "payload_id": payload_id,
-                    "event_type": event_type,
-                    "event_recorded_at": event_recorded_at,
-                }
-            df = pd.DataFrame.from_dict(data_dict, orient="index")
-            print(df.head())
-            # df.to_csv("../output/test.csv", index=False)
+    def read_json(self, path):
+        """parses and converts json to dataframe
+
+        Returns:
+            dataframe: json to dataframe format 
+        """
+        try:
+            with open(path, "r") as f:
+                json_data = json.loads(f.read())
+                print(
+                    f"{len(json_data)} records read from events JSON file")
+
+                data_dict = dict()
+                for i in range(0, len(json_data)):
+                    list_item = json_data[i]
+                    try:
+                        payload_id = list_item["Payload"]["ID"]
+                        event_type = list_item["Type"]
+                        event_recorded_at = list_item["RecordedAt"]
+                    except KeyError as e:
+                        print(f"Key {str(e)} not found in dictionary object")
+                        raise
+
+                    data_dict[i] = {
+                        "payload_id": payload_id,
+                        "event_type": event_type,
+                        "event_recorded_at": event_recorded_at,
+                    }
+                df = pd.DataFrame.from_dict(data_dict, orient="index")
+                print(df.head())
+                # df.to_csv("/output/test.csv", index=False)
+        except Exception as e:
+            print(f"Exception encountered while loading JSON file: {str(e)}")
+            raise
 
         return df
 
-    def write_tosql(self, conn, df):
-        df.to_sql(con=conn, name=self.table_name, schema=self.db_name,
-                  index=False, if_exists='replace')
+    def write_to_db(self, engine, df):
+        """writes dataframe to a table in mysql db
+
+        Args:
+            engine (object): db connection 
+            df (dataframe): events dataframe
+        """
+        try:
+            df.to_sql(con=engine, name=self.table_name, schema=self.db_name,
+                      index=False, if_exists='replace')
+            sql = text(
+                f"select count(*) as record_count from {self.db_name}.{self.table_name}")
+            result = engine.execute(sql)
+        except exc.SQLError as e:
+            print(f'Database operation failed: {str(e)}')
+            raise
+
+        count = [row[0] for row in result][0]
+        print(
+            f"{count} records successfully inserted to {self.table_name} table")
 
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
-    config.read('../config.ini')
+    config.read('config.ini')
     capture = EventCapture(hostname=config['mysqldb']['hostname'],
                            user_name=config['mysqldb']['user_name'],
                            password=config['mysqldb']['password'],
@@ -69,5 +107,7 @@ if __name__ == "__main__":
                            table_name=config['mysqldb']['table_name'],
                            port=int(config['mysqldb']['port']))
     engine = capture.initialise_db_connection()
-    df = capture.read_json()
-    capture.write_tosql(engine, df)
+    path = "data/event-sample.json"
+    df = capture.read_json(path)
+    capture.write_to_db(engine, df)
+    engine.dispose()
---
+++ tests/test_record_event.py
@@ -0,0 +1,26 @@
+import unittest
+import configparser
+from sqlalchemy import exc
+from src.record_event import EventCapture
+
+
+class TestEventCapture(unittest.TestCase):
+
+    def setUp(self):
+        config = configparser.ConfigParser()
+        config.read('../config.ini')
+        self.hostname = "test_host"
+        self.user_name = "test_user"
+        self.password = config['mysqldb']['password']
+        self.db_name = config['mysqldb']['db_name']
+        self.table_name = config['mysqldb']['table_name']
+        self.port = int(config['mysqldb']['port'])
+
+    def test_initialise_db_connection(self):
+        self.assertRaises(exc.SQLAlchemyError,
+                          EventCapture.initialise_db_connection(self))
+
+    def test_read_json(self):
+        path = "../test_data/dummy_events.json"
+        self.assertRaises(Exception,
+                          EventCapture.read_json(self, path))
